6a884a352ac739c6bde0066c65ea9c3a08f2928a
cotzal <domingo.urgell@gmail.com>
2019-01-20 20:20:09 +0100

    Delete ficheros bdd ocupacion.sql

--- ficheros bdd ocupacion.sql
+++
@@ -1,9 +0,0 @@
-
-		select a.FILEID,
-		[FILE_SIZE_MB] =  convert(decimal(12,2),round(a.size/128.000,2)),
-		[SPACE_USED_MB] = convert(decimal(12,2),round(fileproperty(a.name,'SpaceUsed')/128.000,2)),
-		[FREE_SPACE_MB] = convert(decimal(12,2),round((a.size-fileproperty(a.name,'SpaceUsed'))/128.000,2)),
-		NAME ,
-		 FILENAME 
-		from dbo.sysfiles a 
-		
