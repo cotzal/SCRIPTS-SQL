ce574e603545ef97d5a400f0b4b82fee7fc13a36
cotzal <domingo.urgell@gmail.com>
2019-01-20 20:19:35 +0100

    Delete bloqueo a nivel de particion.sql

--- bloqueo a nivel de particion.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE OPERACIONES SET (LOCK_ESCALATION = AUTO)
-
-
-SELECT s.name as schemaname, object_name (t.object_id) as table_name, t.lock_escalation_desc
-FROM sys.tables t, sys.schemas s
-WHERE object_name(t.object_id) = 'SIMULADOR' 
-and s.name = 'dbo' 
-and s.schema_id = t.schema_id 
