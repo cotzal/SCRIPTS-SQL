d4a061cd5973d632f941931eb85190d6cdd6c6c6
Domingo Urgell <durgell@minsait.com>
2022-02-08 12:26:28 +0100

    Create BUSQUEDA DE UN CAMPO EN TABLAS.sql

---
+++ BUSQUEDA DE UN CAMPO EN TABLAS.sql
@@ -0,0 +1,6 @@
+SELECT s.name, t.name, c.* FROM sys.columns AS c
+INNER JOIN sys.tables AS t ON t.object_id = c.object_id
+INNER JOIN sys.schemas AS s ON s.schema_id = t.schema_id
+WHERE c.name like '%__NIF%'
+
+
